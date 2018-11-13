0699d0ccc47c73bf9ac4d02de7fdfb4f4229aec4
majidi <geeekfa@gmail.com>
2018-11-13 15:56:38 +0330

    2

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,6 @@
 const functions = require('firebase-functions');
 const firebaseAdmin = require('firebase-admin');
 var serviceAccount = require('./path/to/serviceAccountKey.json');
-const http = require('http');
-const url = require('url');
 
 firebaseAdmin.initializeApp({
     credential: firebaseAdmin.credential.cert(serviceAccount),
@@ -13,20 +11,6 @@ const fireStore = firebaseAdmin.firestore();
 const setting = { timestampsInSnapshots: true };
 fireStore.settings(setting);
 
-exports.cafes = functions.https.onRequest((request, response) => {
-    fireStore.collection('cafes').get()
-        .then(sns => {
-            var json1 = [];
-            sns.docs.forEach(doc => {
-                json1.push(doc.data());
-            });
-            response.send(json1);
-            return;
-        })
-        .catch(exp => {
-            response.send(exp);
-        });
-});
 //all students
 exports.students = functions.https.onRequest((request, response) => {
     //http://localhost:8010/fir-1-4d266/us-central1/students/a?b=1
@@ -65,19 +49,3 @@ function docForEach(sns) {
     return json1;
    
 }
-
-// Special Students
-exports.studentAhmadi = functions.https.onRequest((request, response) => {
-    fireStore.collection('student').where('family', '==', 'ahmadi').get()
-        .then(sns => {
-            var json1 = [];
-            sns.docs.forEach(doc => {
-                json1.push(doc.data());
-            });
-            response.send(json1);
-            return;
-        })
-        .catch(exp => {
-            response.send(exp);
-        });
-});
